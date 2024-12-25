f97845dbf5d069cdd53a387b707304f711c4e10b
SeokJu Kim <asdfg7123@gmail.com>
2024-12-25 20:35:27 +0900

    [Bronze V] Title: AI 선도대학, Time: 100 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.js
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/32951
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
+const splitPath = process.platform === "linux" ? "\n" : "\r\n";
+let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
+
+const N = +input.shift();
+
+const sol = (num) => {
+  // 아이디어
+  return Math.abs(num - 2024);
+};
+
+console.log(sol(N));
