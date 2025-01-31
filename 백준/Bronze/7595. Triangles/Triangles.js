6ff86531bef0fea30afd0159a8c47e88ec37ff93
SeokJu Kim <asdfg7123@gmail.com>
2025-01-31 17:03:41 +0900

    [Bronze IV] Title: Triangles, Time: 192 ms, Memory: 18956 KB -BaekjoonHub

--- 백준/Bronze/7595. Triangles/Triangles.js
+++ 백준/Bronze/7595. Triangles/Triangles.js
@@ -5,13 +5,16 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
 const splitPath = process.platform === "linux" ? "\n" : "\r\n";
 let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
 
+// const N = +input.shift();
+
 const sol = (list) => {
   // 아이디어
+  const star = "*";
   return list
     .reduce((acc, cur) => {
       const curr = [];
       for (let i = 1; i <= cur; i++) {
-        curr.push("*".repeat(i));
+        curr.push(star.repeat(i));
       }
       return [...acc, ...curr];
     }, [])
