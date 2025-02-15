b8ca55b510ea32498d2ef4fcd2825224fac70ff6
SeokJu Kim <asdfg7123@gmail.com>
2025-02-15 15:24:28 +0900

    [Bronze I] Title: 수열의 변화, Time: 100 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Bronze/1551. 수열의 변화/수열의 변화.js
+++ 백준/Bronze/1551. 수열의 변화/수열의 변화.js
@@ -1,21 +1,21 @@
-// https://www.acmicpc.net/problem/1075
+// https://www.acmicpc.net/problem/1551
 
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "0_example.txt";
 const splitPath = "\n";
 let input = fs.readFileSync(filePath).toString().trim().split(splitPath);
 
-// const N = +input.shift();
-
 const sol = (list) => {
   // 아이디어
   const [N, K] = list.shift().split(" ").map(Number);
   let cur = list.shift().split(",").map(Number);
+  let temp = [];
   for (let i = 0; i < K; i++) {
-    for (let j = cur.length - 1; j >= 0; j--) {
-      cur[j] = cur[j] - cur[j - 1];
+    for (let j = 0; j < cur.length - 1; j++) {
+      temp.push(cur[j + 1] - cur[j]);
     }
-    cur.splice(0, 1);
+    cur = temp;
+    temp = [];
   }
   return cur.join(",");
 };
